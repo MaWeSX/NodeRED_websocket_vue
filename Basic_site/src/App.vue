f8a6c97e5024ca19e6e02b273eb027e76446320d
MaWeSX <mathias.Weber@sixt.com>
2023-08-05 01:16:27 +0200

    3 buttons added

--- Basic_site/src/App.vue
+++ Basic_site/src/App.vue
@@ -3,49 +3,27 @@
 </script>
 
 <template>
-<table class="KNX_overview">
-      <thead>
-        <tr>
-          <th>Address</th>
-          <th>DPT</th>
-          <th>Payload</th>
-          <th>Device Name</th>
-          <th>Last Update</th>
-          <th>Switch</th>
-          <th>status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in payloadData" :key="item.addressRAW">
-          <td>{{ item.address }}</td>
-          <td>{{ item.dpt }}</td>
-          <td>{{ item.payload }}</td>
-          <td>{{ item.devicename }}</td>
-          <td>{{ item.lastupdate }}</td>
-          <td>
-            <span v-if="typeof item.payload === 'boolean'"><button v-on:click="sendMessage( true, item.dpt )" >true</button></span>
-            <span v-if="typeof item.payload === 'boolean'"><button v-on:click="sendMessage( false, item.dpt )" >false</button></span></td>
-          <td>          
-              <span v-if="typeof item.payload === 'boolean'">
-              <span v-if="item.payload" class="dot green"></span>
-              <span v-else class="dot red"></span>
-              </span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+<div class="hero">
+  <div class="custom-button" v-on:click="showValue('1/3/0')">
+    {{ getPayloadValue('1/3/0') }}
+  </div>
+  <div class="custom-button" v-on:click="showValue('1/1/0')">
+    {{ getPayloadValue('1/1/0') }}
+  </div>
+</div>
 </template>
+
 <script>
 export default {
   name: 'App',
-  data: function() {
+  data: function () {
     return {
       connection: null,
       payloadData: [],
     }
   },
   methods: {
-    sendMessage: function(value, device) {
+    sendMessage: function (value, device) {
       const messageObject = {
         value: value,
         device: device
@@ -53,44 +31,65 @@ export default {
       const jsonString = JSON.stringify(messageObject);
       this.connection.send(jsonString);
     },
-    handleWebSocketMessage: function(event) {
+    updateValue: function (address) {
+      // Assuming the item for address "1/3/0" exists in payloadData
+      const item1_3_0 = this.payloadData.find((item) => item.address === "1/3/0");
+      if (item1_3_0) {
+        // Modify the payload of item1_3_0 based on your requirements
+        // For example, toggling the value between true and false
+        item1_3_0.payload = !item1_3_0.payload;
+        console.log('Button hit');
+      };
+    },
+    showValue: function (address) {
+      // Update the content of the custom-button div with the value of item.address = "1/3/0"
+      const value = this.getPayloadValue(address);
+      document.querySelector('.custom-button').textContent = value;
+      console.log('Div hit with: ', value);
+    },
+    getPayloadValue: function (address) {
+      // Assuming the item for address "1/3/0" exists in payloadData
+      const item_x = this.payloadData.find((item) => item.address === address);
+      if (item_x) {
+        //console.log('Value found for: '& item_x)
+        return item_x.payload;
+      }
+      return ''; // Return an empty string if the item is not found
+    },
+    handleWebSocketMessage: function (event) {
       const jsonData = JSON.parse(event.data);
       if (jsonData.topic === "KNXViewer" && jsonData.payload && jsonData.payload.length) {
         // Update the payloadData with the new data from the WebSocket
         this.payloadData = jsonData.payload;
       }
     },
-    
+
   },
-  created: function() {
+  created: function () {
     console.log("Starting Connection to WebSocket Server")
     this.connection = new WebSocket("ws://192.168.1.146:1880/websocket")
-    this.connection.onopen = function(event) {
+    this.connection.onopen = function (event) {
       console.log(event)
       console.log("Successfully connected to the echo WebSocket Server");
     }
     this.connection.onmessage = this.handleWebSocketMessage; // Use the handler to process received data
-    //this.connection.onmessage = function(event) {
-     // console.log(JSON.parse(event.data))
-    
   }
 }
 </script>
+
 <style scoped>
-.dot {
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    display: inline-block;
-    margin-right: 5px;
-  }
-  .green {
-    background-color: green;
-  }
-  .red {
-    background-color: red;
-  }
-  .KNX_overview {
-    width: 100%;
-  }
+
+.hero {
+  grid: 1fr;
+}
+.custom-button {
+  padding: 10px;
+  margin-top: 20px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  width: 120px;
+  height: 120px;
+  border-radius: 20px;
+  box-shadow: 3px 3px 10px 1px;
+}
 </style>
